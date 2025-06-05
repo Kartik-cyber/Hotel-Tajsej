3de3afb1a8862e30a4f94d281b9c32654c8c3c05
KARTIK PATIL <105285588+Kartik-cyber@users.noreply.github.com>
2025-06-05 12:12:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ locoScroll.on("scroll", ScrollTrigger.update);
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  }, 
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
